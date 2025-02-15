84f742cce5e6667f788fd18ae0c757bc686bcae6
DmitryTaushev <123morfix@gmail.com>
2025-02-15 17:35:48 +0500

    Довели до ума

--- task_01/Pizza.py
+++ task_01/Pizza.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from json import load, dump, JSONDecodeError
 
+
 class MenuAndAdd:
-    def __init__(self):
+    def __init__(self, identification):
+        self.identification = identification
         self.menu = {
             "Гавайская": [100, 150, ["сырный соус", "ветчина", "филе цыпленка", "ананасы", "сыр моцарелла", "базилик"]],
             "Грибная": [100, 150, ["чесночный соус", "ветчина", "свежие шампиньоны", "сыр моцарелла", "базилик"]],
@@ -13,24 +15,19 @@ class MenuAndAdd:
                                                    "базилик"]]}
 
         self.ingred_order = {"сырный-соус": 50,
-                        "чесночный-соус": 50,
-                        "пицца-соус":50,
-                        "соус-тысяча-островов":50,
-                        "ветчина": 50,
-                        "филе-цыпленка": 50,
-                        "ананас": 50,
-                        "пепперони": 50,
-                        "сыр-моцарелла": 50,
-                        "тунец": 50,
-                        "маслины": 50,
-                        "базилик": 50,
-                        "свежие-томаты": 50,
-                        "свежие-шампиньоны": 50,}
-    def get_menu(self,menu):
-        menu_list = []
-        for i in menu.keys():
-            menu_list.append(i)
-        return menu_list
+                             "чесночный-соус": 50,
+                             "пицца-соус": 50,
+                             "соус-тысяча-островов": 50,
+                             "ветчина": 50,
+                             "филе-цыпленка": 50,
+                             "ананас": 50,
+                             "пепперони": 50,
+                             "сыр-моцарелла": 50,
+                             "тунец": 50,
+                             "маслины": 50,
+                             "базилик": 50,
+                             "свежие-томаты": 50,
+                             "свежие-шампиньоны": 50, }
 
     def get_ingred(self):
         ingred_list = []
@@ -38,15 +35,96 @@ class MenuAndAdd:
             ingred_list.append(i)
         return ingred_list
 
+    def admin_or_user(self):
+        if self.identification == 'Admin':
+            change_menu = input('Желаете ли изменить меню')
+            if change_menu == 'да':
+                choice = input('Выберите желаемое действие\n1.Удалить позицию\n2.Добавить позицию\n')
+                if choice == '1':
+                    del_position = input('Что желаете убрать?')
+                    try:
+                        del self.menu[del_position]
+
+                    except KeyError:
+                        print('Нет такой пиццы')
+
+                elif choice == '2':
+                    pizza_list_admin = []
+                    ingred_list = []
+                    name = input('Введите название пиццы')
+                    weight = int(input('Введите вес пиццы'))
+                    price = int(input('Введите стоймость пиццы'))
+                    ingredient = input("Введите состав пиццы")
+                    ingred_list.append(ingredient)
+                    pizza_list_admin.append(weight)
+                    pizza_list_admin.append(price)
+                    pizza_list_admin.append(ingred_list)
+                    self.menu[name] = pizza_list_admin
+
+        elif self.identification == 'User':
+            return self.menu
+        return self.menu
+
+    def get_menu(self):
+        menu_list = []
+        for i in self.menu.keys():
+            menu_list.append(i)
+        return menu_list
+
+    def user_choise(self):
+        user_choice = input(f"Выберите пиццу\nEсли хотите собрать свою, напишите Своя пицца:{self.get_menu()}")
+        if user_choice == "Своя пицца":
+
+            user_ingred = input(
+                f"Выберите ингредиенты для своей пиццы:\n{self.get_ingred()}\nИнгредиенты вводить через пробел и ингредиенты из двух слов вводить через тире")
+            pizza_user = PizzaUser(user_ingred, self.ingred_order)
+            pizza_user.get_user_pizza()
+            json_work = WorkWithJson(pizza_user.pizza_user_json_key(), pizza_user.pizza_user_json_value())
+            json_work.create_json()
+
+        else:
+            add_ingred_choice = input(
+                "Выберите добавки, если не надо напишите 'нет'\nВетчина,Больше сыра,Маслины,Пепперони,Филе цыпленка")
+            if add_ingred_choice == 'нет':
+                pizza_menu = PizzaMenu(user_choice, self.menu)
+                pizza_menu.choice_pizza()
+                json_work = WorkWithJson(pizza_menu.pizza_menu_json_key(), pizza_menu.pizza_menu_json_value())
+                json_work.create_json()
+            else:
+                pizza_own = PizzaOwn(user_choice, add_ingred_choice, self.menu)
+                pizza_own.choice_pizza()
+                pizza_own.get_add_ingred()
+                json_work = WorkWithJson(pizza_own.pizza_own_json_key(), pizza_own.pizza_own_json_value())
+                json_work.create_json()
+
+    def show_json(self):
+        import json
+        try:
+            with open('Pizza_and_order.json', 'r', encoding='utf-8') as fp:
+                pizza_list = json.load(fp)
+            print()
+            print('Список проданных пицц')
+            print(pizza_list)
+            print(f'Количество проданных пицц - {len(pizza_list)}')
+            order = 0
+            for i in pizza_list:
+                for key in i:
+                    order += int(i[key])
+            print(f'Выручка - {order}')
+
+        except JSONDecodeError:
+            print('Пока еще нет проданных пицц')
+
+
 class WorkWithJson:
-    def __init__(self,key,value):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
 
     def find_json(self):
         find_json = []
         try:
-            with open('Pizza_and_order.json','r',encoding = 'utf-8') as fp:
+            with open('Pizza_and_order.json', 'r', encoding='utf-8') as fp:
                 find_json = load(fp)
         except JSONDecodeError:
             find_json = 'Error'
@@ -58,10 +136,10 @@ class WorkWithJson:
         data[self.key] = self.value
         json_data.append(data)
         if self.find_json() == 'Error':
-            with open('Pizza_and_order.json','w',encoding = 'utf-8') as fp:
-                dump(json_data,fp,ensure_ascii=False,indent = 2)
+            with open('Pizza_and_order.json', 'w', encoding='utf-8') as fp:
+                dump(json_data, fp, ensure_ascii=False, indent=2)
         elif self.find_json() != 'Error':
-            with open('Pizza_and_order.json','r',encoding = 'utf-8') as fp:
+            with open('Pizza_and_order.json', 'r', encoding='utf-8') as fp:
                 self.json_data = load(fp)
             self.add_json()
 
@@ -69,14 +147,16 @@ class WorkWithJson:
         data = {}
         data[self.key] = self.value
         self.json_data.append(data)
-        with open('Pizza_and_order.json','w',encoding = 'utf-8') as fp:
-            dump(self.json_data,fp,ensure_ascii=False,indent = 2)
+        with open('Pizza_and_order.json', 'w', encoding='utf-8') as fp:
+            dump(self.json_data, fp, ensure_ascii=False, indent=2)
+
 
 class PizzaMenu:
 
-    def __init__(self,user_choice,menu):
+    def __init__(self, user_choice, menu):
         self.menu = menu
         self.user_choice = user_choice
+
     def choice_pizza(self):
         print(f'Вы заказали пиццу {self.user_choice}')
         return self.user_choice
@@ -89,7 +169,7 @@ class PizzaMenu:
 
 
 class PizzaOwn:
-    def __init__(self,user_choice,ingred_choice,menu):
+    def __init__(self, user_choice, ingred_choice, menu):
         self.menu = menu
         self.ingred_choice = ingred_choice
         self.user_choice = user_choice
@@ -109,7 +189,7 @@ class PizzaOwn:
 
 
 class PizzaUser:
-    def __init__(self,user_ingred,ingred_order):
+    def __init__(self, user_ingred, ingred_order):
         self.user_ingred = user_ingred
         self.ingred_order = ingred_order
 
--- task_01/main.py
+++ task_01/main.py
@@ -1,75 +1,9 @@
-from json import JSONDecodeError
+from Pizza import MenuAndAdd
 
-from Pizza import PizzaMenu, PizzaOwn,PizzaUser, WorkWithJson, MenuAndAdd
-import json
 
 if __name__ == "__main__":
-    menu_and_add = MenuAndAdd()
-    menu = []
     identification = input("Идентифицируйте себя: 'Admin','User'")
-    if identification == 'Admin':
-        change_menu = input('Желаете ли изменить меню')
-        if change_menu == 'да':
-            print("1.Удалить позицию\n2.Добавить позицию\n")
-            choice = input('Выберите желаемое действие')
-            if choice == '1':
-                del_position = input('Что желаете убрать?')
-                del menu_and_add.menu[del_position]
-                print(menu_and_add.menu)
-                menu.append(menu_and_add.menu)
-            elif choice == '2':
-                pizza_list_admin = []
-                ingred_list = []
-                name = input('Введите название пиццы')
-                weight = int(input('Введите вес пиццы'))
-                price = int(input('Введите стоймость пиццы'))
-                ingredient = input("Введите состав пиццы")
-                ingred_list.append(ingredient)
-                pizza_list_admin.append(weight)
-                pizza_list_admin.append(price)
-                pizza_list_admin.append(ingred_list)
-                menu_and_add.menu[name] = pizza_list_admin
-                print(menu_and_add.menu)
-                menu.append(menu_and_add.menu)
-    elif identification == 'User':
-        menu = menu_and_add.menu
-        pass
-    user_choice = input(f"Выберите пиццу\nEсли хотите собрать свою, напишите Своя пицца:{menu}")
-    if user_choice == "Своя пицца":
-        """Ингредиенты вводить через пробел и ингредиенты из двух слов вводить через тире """
-        user_ingred = input(f"Выберите ингредиенты для своей пиццы:\n{menu_and_add.get_ingred()}")
-        pizza_user = PizzaUser(user_ingred,menu_and_add.ingred_order)
-        pizza_user.get_user_pizza()
-        json_work = WorkWithJson(pizza_user.pizza_user_json_key(),pizza_user.pizza_user_json_value())
-        json_work.create_json()
-
-    else:
-        add_ingred_choice = input(
-                "Выберите добавки, если не надо напишите 'нет'\nВетчина,Больше сыра,Маслины,Пепперони,Филе цыпленка")
-        if add_ingred_choice == 'нет':
-            pizza_menu = PizzaMenu(user_choice,menu_and_add.menu)
-            pizza_menu.choice_pizza()
-            json_work = WorkWithJson(pizza_menu.pizza_menu_json_key(), pizza_menu.pizza_menu_json_value())
-            json_work.create_json()
-        else:
-            pizza_own = PizzaOwn(user_choice,add_ingred_choice,menu_and_add.menu)
-            pizza_own.choice_pizza()
-            pizza_own.get_add_ingred()
-            json_work = WorkWithJson(pizza_own.pizza_own_json_key(), pizza_own.pizza_own_json_value())
-            json_work.create_json()
-try:
-    with open('Pizza_and_order.json','r',encoding = 'utf-8') as fp:
-        pizza_list = json.load(fp)
-    print()
-    print('Список проданных пицц')
-    print(pizza_list)
-    print(f'Количество проданных пицц - {len(pizza_list)}')
-    order = 0
-    for i in pizza_list:
-        for key in i:
-            order += int(i[key])
-    print(f'Выручка - {order}')
-
-except JSONDecodeError:
-    print('Пока еще нет проданных пицц')
-
+    menu_and_add = MenuAndAdd(identification)
+    menu_and_add.admin_or_user()
+    menu_and_add.user_choise()
+    menu_and_add.show_json()
